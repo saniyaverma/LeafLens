2dbe6d3cee6e14b020504209aa8c189d3a238228
Saniya Verma <saniya.verma1919@gmail.com>
2026-08-03 02:05:28 +0530

    Fix Render build

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python manage.py collectstatic --noinput
+pip install --upgrade pip
+pip install -r requirements.txt
 
+python manage.py collectstatic --noinput
 python manage.py migrate
